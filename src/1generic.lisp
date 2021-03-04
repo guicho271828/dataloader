fd2810314399055f76d75dca45d205f5b011a6ae
Masataro Asai <guicho2.71828@gmail.com>
2021-03-04 14:42:05 -0500

    refactoring: replaced dispatch with CLOS
    Signed-off-by: Masataro Asai <guicho2.71828@gmail.com>

---
+++ src/1generic.lisp
@@ -0,0 +1,88 @@
+
+(in-package :dataloader)
+
+(defun external-format (file)
+  (intern (string-upcase (magicffi:pathname-mime-encoding file)) :keyword))
+
+(defun intern-mime (mime)
+  (let ((s (intern (string-upcase mime) :dataloader.mime)))
+    (export s :dataloader.mime)
+    s))
+
+
+(defparameter *load-keyword-arg-list* nil)
+
+(defgeneric %load (file mime &rest args &key &allow-other-keys))
+
+(defmethod %load ((file string) mime &rest args &key &allow-other-keys)
+  (apply #'%load (pathname file) mime args))
+
+(defmethod %load (file (mime string) &rest args &key &allow-other-keys)
+  (apply #'%load file (intern-mime mime) args))
+
+(defmacro def-load-method ((filevar mime-string-or-strings &rest lambda-list) &body body)
+  "A wrapper over defmethod. Made primarily because defmethod does not support (OR (EQL ...)) or MEMBER as a specializer.
+LAMBDA-LIST contains a keyword argument portion of the congruent lambda list of DEFGENERIC.
+In order to have a compatible lambda-list with the generic function,
+it adds &rest argument if not present,
+and &allow-other-keys when &key is present and &allow-other-keys is missing."
+  ;; 
+  (when (not (member '&rest lambda-list))
+    (with-gensyms (args)
+      (setf lambda-list `(&rest ,args ,@lambda-list))))
+  (when (and (member '&key lambda-list)
+             (not (member '&allow-other-keys lambda-list)))
+    (setf lambda-list `(,@lambda-list &allow-other-keys)))
+  `(progn
+     ,@(when-let ((keyword-arg-list (member '&key lambda-list)))
+         `((add-to-load-keyword-arg-list ',(set-difference keyword-arg-list lambda-list-keywords))))
+     ,@(iter (for mime-string in (ensure-list mime-string-or-strings))
+             (check-type mime-string string)
+             (collecting
+              `(defmethod %load (,filevar (mime (eql ',(intern-mime mime-string))) ,@lambda-list)
+                 ,@(when-let ((vars (mapcar #'car (mapcar #'ensure-list (set-difference lambda-list lambda-list-keywords)))))
+                     `((declare (ignorable ,@vars))))
+                 ,@body)))))
+
+(defun add-to-load-keyword-arg-list (keyword-arg-list)
+  (appendf *load-keyword-arg-list*
+           keyword-arg-list))
+
+
+(defparameter *save-keyword-arg-list* nil)
+
+(defgeneric %save (array file mime &rest args &key &allow-other-keys))
+
+(defmethod %save (array (file string) mime &rest args &key &allow-other-keys)
+  (apply #'%save array (pathname file) mime args))
+
+(defmethod %save (array file (mime string) &rest args &key &allow-other-keys)
+  (apply #'%save array file (intern-mime mime) args))
+
+(defmacro def-save-method ((arrayvar filevar mime-string-or-strings &rest lambda-list) &body body)
+  "A wrapper over defmethod. Made primarily because defmethod does not support (OR (EQL ...)) or MEMBER as a specializer.
+LAMBDA-LIST contains a keyword argument portion of the congruent lambda list of DEFGENERIC.
+In order to have a compatible lambda-list with the generic function,
+it adds &rest argument if not present,
+and &allow-other-keys when &key is present and &allow-other-keys is missing."
+  ;; 
+  (when (not (member '&rest lambda-list))
+    (with-gensyms (args)
+      (setf lambda-list `(&rest ,args ,@lambda-list))))
+  (when (and (member '&key lambda-list)
+             (not (member '&allow-other-keys lambda-list)))
+    (setf lambda-list `(,@lambda-list &allow-other-keys)))
+  `(progn
+     ,@(when-let ((keyword-arg-list (member '&key lambda-list)))
+         `((add-to-save-keyword-arg-list ',(set-difference keyword-arg-list lambda-list-keywords))))
+     ,@(iter (for mime-string in (ensure-list mime-string-or-strings))
+             (check-type mime-string string)
+             (collecting
+              `(defmethod %save (,arrayvar ,filevar (mime (eql ',(intern-mime mime-string))) ,@lambda-list)
+                 ,@(when-let ((vars (mapcar #'car (mapcar #'ensure-list (set-difference lambda-list lambda-list-keywords)))))
+                     `((declare (ignorable ,@vars))))
+                 ,@body)))))
+
+(defun add-to-save-keyword-arg-list (keyword-arg-list)
+  (appendf *save-keyword-arg-list*
+           keyword-arg-list))
---
+++ src/2save.lisp
@@ -0,0 +1,85 @@
+
+(in-package :dataloader)
+
+(def-save-method (array file ("png"))
+  (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede :element-type '(unsigned-byte 8))
+    (png:encode array s)))
+
+(def-save-method (array file ("jpg" "jpeg"))
+  (ematch array
+    ((array :dimensions (list h w _))
+     (cl-jpeg:encode-image file (array-displacement array) 1 h w))))
+
+(def-save-method (array file ("tiff"))
+  (ematch array
+    ((array :dimensions (list h w _))
+     (let ((tiff (make-instance 'retrospectiff:tiff-image
+                                :length h
+                                :width w
+                                :bits-per-sample 8
+                                :samples-per-pixel 1
+                                :data (array-displacement array)
+                                :byte-order tiff::*byte-order*
+                                :min-is-white t)))
+       (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede :element-type '(unsigned-byte 8))
+         (retrospectiff:write-tiff-stream s tiff))))))
+
+(def-save-method (array file ("csv"))
+  (ematch array
+    ((array :dimensions (list h w))
+     (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede)
+       (iter (for i below h)
+             (iter (for j below w)
+                   (unless (first-iteration-p)
+                     (write-char #\, s))
+                   (prin1 (aref array i j) s))
+             (terpri s))))))
+
+(def-save-method (array file ("tsv"))
+  (ematch array
+    ((array :dimensions (list h w))
+     (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede)
+       (iter (for i below h)
+             (iter (for j below w)
+                   (unless (first-iteration-p)
+                     (write-char #\Tab s))
+                   (prin1 (aref array i j) s))
+             (terpri s))))))
+
+
+(def-save-method (array file ("wav"))
+  #+(or)
+  (wav:write-wav-file (list (list :chunk-id "RIFF"
+                                  :chunk-data-size 176436
+                                  :file-type "WAVE")
+                            (list :chunk-id "fmt "
+                                  :chunk-data-size 16
+                                  :chunk-data
+                                  (list :compression-code 1
+                                        :number-of-channels 1
+                                        :sample-rate 44100
+                                        :average-bytes-per-second 88200
+                                        :block-align 2
+                                        :significant-bits-per-sample 16))
+                            (list :chunk-id "data"
+                                  :chunk-data-size (length array)
+                                  :chunk-data array))
+                      file)
+  (wav:write-wav-file array file))
+
+#+(or)
+("audio/flac"
+ (let ((decoder (flac:flac-open file)))
+   (unwind-protect
+        (progn
+          
+          )
+     (flac:flac-close decoder))))
+
+#+(or)
+("image/x-ms-bmp"
+ (png::decode-file file))
+
+#+(or)
+("image/gif"
+ (retrospectiff:read-tiff-file file))
