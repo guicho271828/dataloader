ebe72ce8af7dce9b8c1b9ee9b64dab59f107227b
Masataro Asai <guicho2.71828@gmail.com>
2021-03-05 00:26:11 -0500

    trivial optimization
    Signed-off-by: Masataro Asai <guicho2.71828@gmail.com>

--- src/2load.lisp
+++ src/2load.lisp
@@ -3,7 +3,14 @@
 
 (def-load-method (file "image/png" &rest args)
   (with-open-file (s file :direction :input :element-type '(unsigned-byte 8))
-    (asarray (png:decode s))))
+    (let ((data (png:decode s)))
+      ;; this is a multidimensional simple array. Lets convert it to numcl array
+      (multiple-value-bind (array base-array) (numcl:empty-like data)
+        ;; base-array is a simple array
+        (dotimes (i (array-total-size data))
+          (setf (row-major-aref base-array i)
+                (row-major-aref data i)))
+        array))))
 
 (def-load-method (file "image/jpeg" &rest args &key buffer (colorspace-conversion t) cached-source-p (decode-frame t))
   (multiple-value-bind (array height width components transform) (apply #'cl-jpeg:decode-image file :allow-other-keys t args)
@@ -20,12 +27,19 @@
                                :width w
                                :samples-per-pixel c
                                :bits-per-sample n)
+     ;; the biggest flaw of retrospectiff is that the data array is untyped.
+     ;; let's convert it into a typed numcl array.
+
      ;; n could be an integer or a sequence of integers.
      (when (typep n 'sequence)
        (assert (= (reduce #'max n) (reduce #'min n))) ; length of n is below 4 (channel size), so this is safe.
        (setf n (reduce #'max n)))
-     (reshape (asarray data :type `(unsigned-byte ,n))
-              (list h w c)))))
+     (multiple-value-bind (array base-array) (numcl:empty (list h w c) :type `(unsigned-byte ,n))
+       ;; base-array is a simple array
+       (dotimes (i (length base-array))
+         (setf (row-major-aref base-array i)
+               (row-major-aref data i)))
+       array))))
 
 (def-load-method (file ("application/csv"
                         "application/x-csv"
