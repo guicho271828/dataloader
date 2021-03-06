eedec1a030974b16742dfd4e9b00f35b783e8306
Masataro Asai <guicho2.71828@gmail.com>
2021-03-06 14:41:57 -0500

    array -> data ??

--- src/2save.lisp
+++ src/2save.lisp
@@ -1,56 +1,56 @@
 
 (in-package :dataloader)
 
-(define-save-method (array file ("png"))
+(define-save-method (data file ("png"))
   (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede :element-type '(unsigned-byte 8))
-    (png:encode (numcl:to-simple-array array) s)))
+    (png:encode (numcl:to-simple-array data) s)))
 
-(define-save-method (array file ("jpg" "jpeg"))
-  (ematch array
+(define-save-method (data file ("jpg" "jpeg"))
+  (ematch data
     ((array :dimensions (list h w c) :displaced-to d)
      (cl-jpeg:encode-image file d c h w))))
 
-(define-save-method (array file ("tiff"))
-  (ematch array
+(define-save-method (data file ("tiff"))
+  (ematch data
     ((array :dimensions (list h w c))
      (let ((tiff (make-instance 'retrospectiff:tiff-image
                                 :length h
                                 :width w
                                 :bits-per-sample 8
                                 :samples-per-pixel c
-                                :data (array-displacement array)
+                                :data (array-displacement data)
                                 :byte-order tiff::*byte-order*
                                 :min-is-white t)))
        (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede :element-type '(unsigned-byte 8))
          (retrospectiff:write-tiff-stream s tiff))))))
 
-(define-save-method (array file ("csv"))
-  (ematch array
+(define-save-method (data file ("csv"))
+  (ematch data
     ((array :dimensions (list h w))
      (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede)
        (iter (for i below h)
              (iter (for j below w)
                    (unless (first-iteration-p)
                      (write-char #\, s))
-                   (prin1 (aref array i j) s))
+                   (prin1 (aref data i j) s))
              (terpri s))))))
 
-(define-save-method (array file ("tsv"))
-  (ematch array
+(define-save-method (data file ("tsv"))
+  (ematch data
     ((array :dimensions (list h w))
      (with-open-file (s file :direction :output :if-does-not-exist :create :if-exists :supersede)
        (iter (for i below h)
              (iter (for j below w)
                    (unless (first-iteration-p)
                      (write-char #\Tab s))
-                   (prin1 (aref array i j) s))
+                   (prin1 (aref data i j) s))
              (terpri s))))))
 
 
 ;; Using riff and wav requires reading
 ;; Multimedia Programming Interface and Data Specifications 1.0.
 
-(define-save-method (array file ("wav")
+(define-save-method (data file ("wav")
                         &key
                         (bits-per-second 44100))
   (flet ((writer (bits-per-sample data-size data)
@@ -85,32 +85,32 @@
                           :chunk-data-size data-size ;2
                           :chunk-data data))
               stream :chunk-data-writer (wav::wrap-format-chunk-data-writer)))))
-    (ematch array
+    (ematch data
       ((array :dimensions (list size)
               :element-type (list 'unsigned-byte 8))
-       (writer 8 size array))
+       (writer 8 size data))
       
       ((array :dimensions (list size 2)
               :element-type (list 'unsigned-byte 8))
-       (writer 8 (* 2 size) (numcl:flatten array)))
+       (writer 8 (* 2 size) (numcl:flatten data)))
 
       ((array :dimensions (list size)
               :element-type (list 'unsigned-byte 16))
-       (let ((array2 (make-array (* size 2) :element-type '(unsigned-byte 8))))
+       (let ((data2 (make-array (* size 2) :element-type '(unsigned-byte 8))))
          (dotimes (i size)
-           (setf (aref array2 (* 2 i))      (logand #xFF (aref array i))
-                 (aref array2 (1+ (* 2 i))) (ash (aref array i) -8)))
-         (writer 16 (* 2 size) array2)))
+           (setf (aref data2 (* 2 i))      (logand #xFF (aref data i))
+                 (aref data2 (1+ (* 2 i))) (ash (aref data i) -8)))
+         (writer 16 (* 2 size) data2)))
 
       ((array :dimensions (list size 2)
               :element-type (list 'unsigned-byte 16))
-       (let ((array2 (make-array (* size 4) :element-type '(unsigned-byte 8))))
+       (let ((data2 (make-array (* size 4) :element-type '(unsigned-byte 8))))
          (dotimes (i size)
-           (setf (aref array2 (+ 0 (* 4 i))) (logand #xFF (aref array i 0))
-                 (aref array2 (+ 1 (* 4 i))) (ash (aref array i 0) -8)
-                 (aref array2 (+ 2 (* 4 i))) (logand #xFF (aref array i 1))
-                 (aref array2 (+ 3 (* 4 i))) (ash (aref array i 1) -8)))
-         (writer 16 (* 4 size) array2))))))
+           (setf (aref data2 (+ 0 (* 4 i))) (logand #xFF (aref data i 0))
+                 (aref data2 (+ 1 (* 4 i))) (ash (aref data i 0) -8)
+                 (aref data2 (+ 2 (* 4 i))) (logand #xFF (aref data i 1))
+                 (aref data2 (+ 3 (* 4 i))) (ash (aref data i 1) -8)))
+         (writer 16 (* 4 size) data2))))))
 
 #+(or)
 ("audio/flac"
