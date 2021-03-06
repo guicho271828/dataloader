c26b1f49acbedc1449d6cc1502e164455dab50e0
Masataro Asai <guicho2.71828@gmail.com>
2021-03-06 15:04:40 -0500

    documentation

--- src/3api.lisp
+++ src/3api.lisp
@@ -4,6 +4,7 @@
 (defun load (file &rest args
              &key (mime (magicffi:pathname-mime-type file))
                . #.*load-keyword-arg-list*)
+  "Loads a file and returns a numcl-compatible array. The file format is detected by libmagic, but it can optionally be specified."
   (declare (ignore . #.(mapcar #'car (mapcar #'ensure-list *load-keyword-arg-list*))))
   (apply #'perform-load file mime args))
 
@@ -11,6 +12,7 @@
 (defun save (array file &rest args
              &key (mime (pathname-type file))
                . #.*save-keyword-arg-list*)
+  "This function saves the array into a file, using the format specified by the file name extension or by the mime type."
   (declare (ignore . #.(mapcar #'car (mapcar #'ensure-list *save-keyword-arg-list*))))
   (apply #'perform-save array file mime args))
 
